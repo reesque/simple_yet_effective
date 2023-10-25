4fe952d750ab87fdc1af69ff7b0f5f69e21c976f
Khoa Nguyen <akhoa.nv@gmail.com>
2023-10-25 02:07:54 -0400

    Added animations and different state icons to various modules. Fixed module spacing

--- src/polybar/scripts/bluetooth/bluetooth.sh
+++ src/polybar/scripts/bluetooth/bluetooth.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo 
+  echo %{F#707880}%{T2}%{T-}%{T-}%{T0} Off%{T-}%{F-}
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
-    echo 
+    echo %{F#C5C8C6}%{T2}%{T-}%{T-}%{T0} On%{T-}%{F-}
   fi
-  echo 
+  echo %{F#C5C8C6}%{T2}%{T-}%{T-}%{T0} Connected%{T-}%{F-}
 fi
